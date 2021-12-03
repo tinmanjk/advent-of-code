5faba9e7b4e2f8507bd241919fbb48ec760b4a67
tinmanjk <tinmanjk@mail.com>
2021-12-03 10:18:06 +0200

    2021: Refactored day 03

--- 2021/03/Go/2021_03.go
+++ 2021/03/Go/2021_03.go
@@ -9,19 +9,52 @@ import (
 	"strings"
 )
 
-const inputPath = "../input0.txt"
+const inputPath = "../input.txt"
 
 func main() {
 	lines := returnSliceOfLinesFromFile(inputPath)
-	// lines := returnSliceOfLinesFromFile()
-	result := int(task02(lines)) // possible conversion stuff
+	var result int
+
+	result = task01(lines)
 	fmt.Println(result)
 
-	// result = task02(numbers)
-	// fmt.Println(result)
+	result = task02(lines) // possible conversion stuff
+	fmt.Println(result)
 }
 
-func task02(lines []string) (result float64) {
+func task01(lines []string) (result int) {
+	totalLines := len(lines)
+	lineLength := len(lines[0]) // assumption equal line-length
+
+	gamma := ""   // most common bit
+	epsilon := "" // least common bit
+	for x := 0; x < lineLength; x++ {
+		column := make([]rune, 0)
+		for y := 0; y < totalLines; y++ {
+			column = append(column, rune(lines[y][x]))
+		}
+		// no equally common ... so assumption is that epsilon is the inverted gamma
+
+		count0, count1 := returnSliceCountsForOnesAndZeroes(column)
+		if count1 >= count0 {
+			gamma += "1"
+			epsilon += "0"
+		} else {
+			gamma += "0"
+			epsilon += "1"
+		}
+	}
+
+	var gammaFloat, epsilonFloat float64
+	gammaFloat = convertFromBinary(gamma)
+	epsilonFloat = convertFromBinary(epsilon)
+
+	// TODO: check conversion
+	result = int(gammaFloat * epsilonFloat)
+	return result
+}
+
+func task02(lines []string) (result int) {
 
 	totalLines := len(lines)
 	lineLength := len(lines[0]) // assumption equal line-length
@@ -72,10 +105,38 @@ func task02(lines []string) (result float64) {
 	oxygenFloat = convertFromBinary(oxygen)
 	co2Float = convertFromBinary(co2)
 
-	return oxygenFloat * co2Float
+	// TODO: check conversion
+	result = int(oxygenFloat * co2Float)
+	return result
 }
 
-func returnCountsForOnesAndZeroes(lines map[int]rune) (count0 int, count1 int) {
+func oxygenFilterLogic(lineRuneMap map[int]rune, hashMapIndeces *map[int]int) {
+	count0, count1 := returnMapCountsForOnesAndZeroes(lineRuneMap)
+
+	var oxygenFilter rune
+	if count1 >= count0 {
+		oxygenFilter = '1'
+	} else {
+		oxygenFilter = '0'
+	}
+
+	filterOutHashmap(lineRuneMap, hashMapIndeces, oxygenFilter)
+}
+
+func co2FilterLogic(lineRuneMap map[int]rune, hashMapIndeces *map[int]int) {
+	count0, count1 := returnMapCountsForOnesAndZeroes(lineRuneMap)
+
+	var co2Filter rune
+	if count0 <= count1 {
+		co2Filter = '0'
+	} else {
+		co2Filter = '1'
+	}
+
+	filterOutHashmap(lineRuneMap, hashMapIndeces, co2Filter)
+}
+
+func returnSliceCountsForOnesAndZeroes(lines []rune) (count0 int, count1 int) {
 	for _, v := range lines {
 		switch v {
 		case '1':
@@ -87,38 +148,24 @@ func returnCountsForOnesAndZeroes(lines map[int]rune) (count0 int, count1 int) {
 	return
 }
 
-func filterHashmap(lineRuneMap map[int]rune, hashmapIndeces *map[int]int, filterOut rune) {
+func returnMapCountsForOnesAndZeroes(lines map[int]rune) (count0 int, count1 int) {
+	for _, v := range lines {
+		switch v {
+		case '1':
+			count1++
+		case '0':
+			count0++
+		}
+	}
+	return
+}
+
+func filterOutHashmap(lineRuneMap map[int]rune, hashmapIndeces *map[int]int, filterOut rune) {
 	for k, v := range lineRuneMap {
 		if v != filterOut {
 			delete(*hashmapIndeces, k)
 		}
 	}
-}
-
-func oxygenFilterLogic(lineRuneMap map[int]rune, hashMapIndeces *map[int]int) {
-	count0, count1 := returnCountsForOnesAndZeroes(lineRuneMap)
-
-	var oxygenFilter rune
-	if count1 >= count0 {
-		oxygenFilter = '1'
-	} else {
-		oxygenFilter = '0'
-	}
-
-	filterHashmap(lineRuneMap, hashMapIndeces, oxygenFilter)
-}
-
-func co2FilterLogic(lineRuneMap map[int]rune, hashMapIndeces *map[int]int) {
-	count0, count1 := returnCountsForOnesAndZeroes(lineRuneMap)
-
-	var co2Filter rune
-	if count0 <= count1 {
-		co2Filter = '0'
-	} else {
-		co2Filter = '1'
-	}
-
-	filterHashmap(lineRuneMap, hashMapIndeces, co2Filter)
 }
 
 func convertFromBinary(binaryString string) (result float64) {
