fa93b0989f31eb8065f7ad8b1ea32a957a45c206
tinmanjk <tinmanjk@mail.com>
2021-12-04 08:12:16 +0200

    2021:04 Raw Solutions

--- 2021/04/Go/2021_04.go
+++ 2021/04/Go/2021_04.go
@@ -9,27 +9,151 @@ import (
 	"strings"
 )
 
-const inputPath = "../input0.txt"
+const inputPath = "../input.txt"
+
+func RemoveIndex(s []string, index int) []string {
+	return append(s[:index], s[index+1:]...)
+}
 
 func main() {
 	lines := returnSliceOfLinesFromFile(inputPath)
+	randomNumbersStrings := strings.Split(lines[0], ",")
+
+	randomNumbers := make([]int, len(randomNumbersStrings))
+	for i := 0; i < len(randomNumbersStrings); i++ {
+		randomNumbers[i], _ = strconv.Atoi(randomNumbersStrings[i])
+	}
+
+	// number of matrices
+	numberOfMatrices := ((len(lines) - 1) / 6)
+	// numberOfMatrices := 99
+
+	// matrices
+	listOfMatrices := make([][][]string, numberOfMatrices)
+	indexMatrix := -1
+	indexLine := 0
+	for i := 1; i < len(lines); i++ {
+		if lines[i] == "" {
+			indexMatrix++
+			listOfMatrices[indexMatrix] = make([][]string, 5)
+			indexLine = 0
+			continue
+		}
+		listOfMatrices[indexMatrix][indexLine] = strings.Split(lines[i], " ")
+		if len(listOfMatrices[indexMatrix][indexLine]) != 5 {
+			for i := 0; i < len(listOfMatrices[indexMatrix][indexLine]); i++ {
+				if listOfMatrices[indexMatrix][indexLine][i] == "" {
+					listOfMatrices[indexMatrix][indexLine] = RemoveIndex(listOfMatrices[indexMatrix][indexLine], i)
+					i--
+				}
+
+			}
+		}
+		indexLine++
+	}
+
 	var result int
-
-	result = task01(lines)
+	// result = task01(randomNumbersStrings, listOfMatrices)
 	fmt.Println(result)
 
-	result = task02(lines)
+	result = task02(randomNumbersStrings, listOfMatrices)
+
 	fmt.Println(result)
 }
 
-func task01(lines []string) (result int) {
+func task01(randomNumbers []string, listOfMatrices [][][]string) (result int) {
+
+	for i := 0; i < len(randomNumbers); i++ {
+		randomNumber := randomNumbers[i]
+		// traverse the matrices
+		for j := 0; j < len(listOfMatrices); j++ {
+			// inside a matrix by row
+			for k := 0; k < 5; k++ {
+				// set empty as easiest
+				// inside a row by column
+				for l := 0; l < 5; l++ {
+					if listOfMatrices[j][k][l] == randomNumber {
+						listOfMatrices[j][k][l] = ""
+						if checkBingo(listOfMatrices[j]) {
+							result = calculateScore(randomNumber, listOfMatrices[j])
+							return
+						}
+					}
+				}
+
+			}
+		}
+	}
 	return
 }
 
-func task02(lines []string) (result int) {
+func checkBingo(matrix [][]string) bool {
+	// row
+	// concatenate
+	for i := 0; i < 5; i++ {
+		rowConcat := matrix[i][0] + matrix[i][1] + matrix[i][2] + matrix[i][3] + matrix[i][4]
+		if rowConcat == "" {
+			return true
+		}
+		colConcat := matrix[0][i] + matrix[1][i] + matrix[2][i] + matrix[3][i] + matrix[4][i]
+		if colConcat == "" {
+			return true
+		}
+	}
+	return false
+}
+
+func calculateScore(randomNumber string, matrix [][]string) (score int) {
+
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			if matrix[i][j] != "" {
+				converted, _ := strconv.Atoi(matrix[i][j])
+				score += converted
+			}
+		}
+	}
+	convertedRandomNumber, _ := strconv.Atoi(randomNumber)
+	score *= convertedRandomNumber
 	return
 }
 
+// list of matrices
+
+func task02(randomNumbers []string, listOfMatrices [][][]string) (result int) {
+
+	numberMatrices := 0
+	alreadyWon := make(map[int]int, len(listOfMatrices))
+	for i := 0; i < len(randomNumbers); i++ {
+		randomNumber := randomNumbers[i]
+		// traverse the matrices
+		for j := 0; j < len(listOfMatrices); j++ {
+			if _, ok := alreadyWon[j]; ok {
+				continue
+			}
+			// inside a matrix by row
+			for k := 0; k < 5; k++ {
+				// set empty as easiest
+				// inside a row by column
+				for l := 0; l < 5; l++ {
+					if listOfMatrices[j][k][l] == randomNumber {
+						listOfMatrices[j][k][l] = ""
+						if checkBingo(listOfMatrices[j]) {
+							result = calculateScore(randomNumber, listOfMatrices[j])
+							alreadyWon[j] = j
+							numberMatrices++
+							if numberMatrices == len(listOfMatrices) {
+								return
+							}
+						}
+					}
+				}
+
+			}
+		}
+	}
+	return
+}
 func convertFromBinary(binaryString string) (result int) {
 	for i := 0; i < len(binaryString); i++ {
 		if !(binaryString[i] == '0' || binaryString[i] == '1') {
