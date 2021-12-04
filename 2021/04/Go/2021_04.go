47724106e6a19bcb658948ff6ce840d322e43808
tinmanjk <tinmanjk@mail.com>
2021-12-04 08:35:05 +0200

    2021:04 refactored solutions

--- 2021/04/Go/2021_04.go
+++ 2021/04/Go/2021_04.go
@@ -11,25 +11,30 @@ import (
 
 const inputPath = "../input.txt"
 
-func RemoveIndex(s []string, index int) []string {
-	return append(s[:index], s[index+1:]...)
-}
-
 func main() {
 	lines := returnSliceOfLinesFromFile(inputPath)
-	randomNumbersStrings := strings.Split(lines[0], ",")
+	randomNumbersStrings, listOfMatrices := parseInput(lines)
+	var result int
+	result = task01(randomNumbersStrings, listOfMatrices)
+	fmt.Println(result)
+	randomNumbersStrings, listOfMatrices = parseInput(lines)
+	result = task02(randomNumbersStrings, listOfMatrices)
+
+	fmt.Println(result)
+}
+
+func parseInput(lines []string) (randomNumbersStrings []string, listOfMatrices [][][]string) {
+	randomNumbersStrings = strings.Split(lines[0], ",")
 
 	randomNumbers := make([]int, len(randomNumbersStrings))
 	for i := 0; i < len(randomNumbersStrings); i++ {
 		randomNumbers[i], _ = strconv.Atoi(randomNumbersStrings[i])
 	}
 
-	// number of matrices
-	numberOfMatrices := ((len(lines) - 1) / 6)
-	// numberOfMatrices := 99
+	numberOfMatrices := (len(lines) - 1) / 6
 
 	// matrices
-	listOfMatrices := make([][][]string, numberOfMatrices)
+	listOfMatrices = make([][][]string, numberOfMatrices)
 	indexMatrix := -1
 	indexLine := 0
 	for i := 1; i < len(lines); i++ {
@@ -40,10 +45,11 @@ func main() {
 			continue
 		}
 		listOfMatrices[indexMatrix][indexLine] = strings.Split(lines[i], " ")
+		// special case for single digit numbers having two spaces - empty space leftover
 		if len(listOfMatrices[indexMatrix][indexLine]) != 5 {
 			for i := 0; i < len(listOfMatrices[indexMatrix][indexLine]); i++ {
 				if listOfMatrices[indexMatrix][indexLine][i] == "" {
-					listOfMatrices[indexMatrix][indexLine] = RemoveIndex(listOfMatrices[indexMatrix][indexLine], i)
+					listOfMatrices[indexMatrix][indexLine] = removeIndex(listOfMatrices[indexMatrix][indexLine], i)
 					i--
 				}
 
@@ -52,13 +58,7 @@ func main() {
 		indexLine++
 	}
 
-	var result int
-	// result = task01(randomNumbersStrings, listOfMatrices)
-	fmt.Println(result)
-
-	result = task02(randomNumbersStrings, listOfMatrices)
-
-	fmt.Println(result)
+	return
 }
 
 func task01(randomNumbers []string, listOfMatrices [][][]string) (result int) {
@@ -88,14 +88,15 @@ func task01(randomNumbers []string, listOfMatrices [][][]string) (result int) {
 }
 
 func checkBingo(matrix [][]string) bool {
-	// row
-	// concatenate
-	for i := 0; i < 5; i++ {
-		rowConcat := matrix[i][0] + matrix[i][1] + matrix[i][2] + matrix[i][3] + matrix[i][4]
+	for i := 0; i < len(matrix); i++ {
+		var rowConcat, colConcat string
+		for j := 0; j < len(matrix); j++ {
+			rowConcat += matrix[i][j]
+			colConcat += matrix[j][i]
+		}
 		if rowConcat == "" {
 			return true
 		}
-		colConcat := matrix[0][i] + matrix[1][i] + matrix[2][i] + matrix[3][i] + matrix[4][i]
 		if colConcat == "" {
 			return true
 		}
@@ -105,8 +106,8 @@ func checkBingo(matrix [][]string) bool {
 
 func calculateScore(randomNumber string, matrix [][]string) (score int) {
 
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix); j++ {
 			if matrix[i][j] != "" {
 				converted, _ := strconv.Atoi(matrix[i][j])
 				score += converted
@@ -117,8 +118,6 @@ func calculateScore(randomNumber string, matrix [][]string) (score int) {
 	score *= convertedRandomNumber
 	return
 }
-
-// list of matrices
 
 func task02(randomNumbers []string, listOfMatrices [][][]string) (result int) {
 
@@ -154,18 +153,6 @@ func task02(randomNumbers []string, listOfMatrices [][][]string) (result int) {
 	}
 	return
 }
-func convertFromBinary(binaryString string) (result int) {
-	for i := 0; i < len(binaryString); i++ {
-		if !(binaryString[i] == '0' || binaryString[i] == '1') {
-			log.Panic("Only accept 0 and 1 in string")
-		}
-		result <<= 1 // make place to the left
-		if binaryString[i] == '1' {
-			result |= 1
-		}
-	}
-	return
-}
 
 func returnSliceOfLinesFromFile(filePath string) (sliceOfLines []string) {
 	file, err := os.Open(filePath)
@@ -184,48 +171,6 @@ func returnSliceOfLinesFromFile(filePath string) (sliceOfLines []string) {
 	return lines
 }
 
-func splitLine(line string) (firstNumber int, secondNumber int,
-	char rune, password string) {
-
-	// Example line: 5-6 v: hvvgvrm
-	lineSplit := strings.Split(line, " ") // should be 3
-	numbers := strings.Split(lineSplit[0], "-")
-	// TODO: Better Error handling
-	firstNumber, err := strconv.Atoi(numbers[0])
-	if err != nil {
-		log.Panic(err)
-	}
-	secondNumber, err = strconv.Atoi(numbers[1])
-	if err != nil {
-		log.Panic(err)
-	}
-
-	// use if there are multi-byte unicode chars
-	for _, r := range lineSplit[1] {
-		char = r
-		break
-	}
-
-	password = lineSplit[2]
-	return
-}
-
-func returnSliceOfIntsFromFile(filePath string) (sliceOfLines []int) {
-	slicesOfLines := returnSliceOfLinesFromFile(filePath)
-
-	lines := make([]int, 0, len(slicesOfLines))
-	for i := 0; i < len(slicesOfLines); i++ {
-		trimmed := strings.TrimRight(slicesOfLines[i], "\n ")
-		if trimmed == "" {
-			continue
-		}
-		// TODO better Error handling Atoi
-		number, err := strconv.Atoi(trimmed)
-		if err != nil {
-			log.Panic(err)
-		}
-		lines = append(lines, number)
-	}
-
-	return lines
+func removeIndex(s []string, index int) []string {
+	return append(s[:index], s[index+1:]...)
 }
