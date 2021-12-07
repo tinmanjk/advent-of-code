374715b20308a23e4ca3bbd36771e7625673031c
tinmanjk <tinmanjk@mail.com>
2021-12-07 12:59:36 +0200

    2021:07 Added analytic solution

--- 2021/07/Go/2021_07.go
+++ 2021/07/Go/2021_07.go
@@ -6,34 +6,84 @@ import (
 	"log"
 	"math"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-const inputPath = "../input.txt"
+const inputPath = "../input0.txt"
 
 func main() {
 	lines := returnSliceOfLinesFromFile(inputPath)
 	crabHorizontalPosition := parseInput(lines)
+
 	var result int
 
-	result = findResult(crabHorizontalPosition, false)
+	// brute force
+	result = findResult(crabHorizontalPosition, false, true)
 	fmt.Println(result)
 
-	result = findResult(crabHorizontalPosition, true)
+	result = findResult(crabHorizontalPosition, true, true)
 	fmt.Println(result)
 
-	// result = findResult(fishTimeToNew, 80)
-	// fmt.Println(result)
+	// median and min sum triangular distance
+	result = findResult(crabHorizontalPosition, false, false)
+	fmt.Println(result)
+
+	result = findResult(crabHorizontalPosition, true, false)
+	fmt.Println(result)
+
 }
 
-func findAverage(sliceOfUints []int) (result int) {
+func parseInput(slicesOfLines []string) (sliceOfInts []int) {
+	line := slicesOfLines[0]
+	splitted := strings.Split(line, ",")
+	sliceOfInts = make([]int, len(splitted))
+	for i := 0; i < len(splitted); i++ {
+		sliceOfInts[i], _ = strconv.Atoi(splitted[i])
+	}
+	return
+}
+
+func findMedian(sliceOfInts []int) (median1 int, median2 int) {
+
+	// not to produce side-effect for initial slice
+	copySlice := make([]int, len(sliceOfInts))
+	copy(copySlice, sliceOfInts)
+	sort.Ints(copySlice)
+	middleIndex := len(copySlice) / 2
+
+	// odd number
+	if len(copySlice)&1 == 1 {
+		median1 = copySlice[middleIndex]
+		median2 = median1
+		return
+	}
+
+	// even number
+	median1 = copySlice[middleIndex]
+	median2 = copySlice[middleIndex+1]
+	return
+}
+
+// n²+n / 2 -> where n = |ai - x|
+// differentiating the sum and taking into account modulus
+// x = sum/N + sign(x-ai)/2N / max sign(x-ai) -> N
+// => x = average +-1/2 or two values to check floor(average) + ceiling(average)
+func findMinTriangularDistance(sliceOfInts []int) (floorAverage int, ceilingAverage int) {
+	average := findAverage(sliceOfInts)
+	ceilingAverage = int(math.Ceil(average))
+	floorAverage = int(math.Floor(average))
+	return
+}
+
+func findAverage(sliceOfUints []int) (result float64) {
 	var sum int
 	for _, v := range sliceOfUints {
 		sum += v
 	}
-
-	return sum / len(sliceOfUints)
+	result = float64(sum) / float64(len(sliceOfUints))
+	return
 }
 
 func findMinAndMax(sliceOfInts []int) (min int, max int) {
@@ -52,29 +102,39 @@ func findMinAndMax(sliceOfInts []int) (min int, max int) {
 	return
 }
 
-func parseInput(slicesOfLines []string) (sliceOfInts []int) {
-	line := slicesOfLines[0]
-	splitted := strings.Split(line, ",")
-	sliceOfInts = make([]int, len(splitted))
-	for i := 0; i < len(splitted); i++ {
-		sliceOfInts[i], _ = strconv.Atoi(splitted[i])
-	}
-	return
-}
+func findResult(numbers []int, variableRate bool, bruteforce bool) (result int) {
 
-// bruteforcing
-func findResult(numbers []int, variableRate bool) (result int) {
-	min, max := findMinAndMax(numbers)
+	var lowerBound, upperBound int
+	if bruteforce {
+		lowerBound, upperBound = findMinAndMax(numbers)
+	} else {
+		if variableRate {
+			//https://en.wikipedia.org/wiki/Triangular_number
+			lowerBound, upperBound = findMinTriangularDistance(numbers)
+		} else {
+			//https://math.stackexchange.com/questions/113270/the-median-minimizes-the-sum-of-absolute-deviations-the-ell-1-norm
+			// ∑i=1N|si−x| -> minimize by ∑sign(si-x) = 0 -> which is median
+			lowerBound, upperBound = findMedian(numbers)
+			// could be unnecessary to assign to both - for consistency
+			// alternatively lowerbound, _ , upperBound = lowerBound
+		}
+	}
+
 	results := make(map[int]int)
-	for i := min; i <= max; i++ {
+	for i := lowerBound; i <= upperBound; i++ {
 		for j := 0; j < len(numbers); j++ {
 			distance := int(math.Abs(float64(numbers[j] - i)))
 			cost := 0
 			if variableRate {
-				additional := 1
-				for k := 0; k < distance; k++ {
-					cost += additional
-					additional++
+				if bruteforce {
+					additional := 1
+					for k := 0; k < distance; k++ {
+						cost += additional
+						additional++
+					}
+				} else {
+					// triangular numbers n(n+1)/2 -> distsance = n = |ai-x|
+					cost = distance * (distance + 1) / 2
 				}
 			} else {
 				cost = distance
@@ -83,6 +143,7 @@ func findResult(numbers []int, variableRate bool) (result int) {
 		}
 	}
 
+	// Find minimum in hashmap
 	result = math.MaxInt32
 	for _, v := range results {
 		if result >= v {
