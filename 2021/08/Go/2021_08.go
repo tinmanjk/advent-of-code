1fdcb93ca0512da5005db0ef427e68c5bc11b291
tinmanjk <tinmanjk@mail.com>
2021-12-09 02:11:02 +0200

    2021:08 Shorted diff-based solution

--- 2021/08/Go/2021_08.go
+++ 2021/08/Go/2021_08.go
@@ -49,9 +49,7 @@ func parseInput(slicesOfLines []string) (sliceOfInputLines []inputLine) {
 func findResult(solutionData []inputLine, partOne bool) (result int) {
 
 	for i := 0; i < len(solutionData); i++ {
-		digitToSignalPatternMap := createDigitToSignalMap(solutionData[i].tenUniqueSignalPattern)
-		// signalwire is e.g. 'b' -> segment is also 'b' - do not match
-		signalWireToSegmentMap := createSignalWireToSegmentMap(digitToSignalPatternMap)
+		signalWireToSegmentMap := createSignalWireToSegmentMap(solutionData[i].tenUniqueSignalPattern)
 
 		multiplier := 1000
 		for k := 0; k < 4; k++ {
@@ -71,15 +69,69 @@ func findResult(solutionData []inputLine, partOne bool) (result int) {
 	return
 }
 
-func decodeDigit(code string, segmentsDecodeMap map[rune]rune) (digit int) {
+func createSignalWireToSegmentMap(tenPattern []string) (decodeTemplate map[byte]byte) {
+	var cf, acf, bcdf string // 1, 7, 4
+	twoThreeFive := make([]string, 0)
 
-	decodedRunes := make([]rune, 0)
-	for _, v := range code {
-		decodedRunes = append(decodedRunes, segmentsDecodeMap[v])
+	for _, s := range tenPattern {
+		switch len(s) {
+		case 2:
+			cf = s
+		case 3:
+			acf = s
+		case 4:
+			bcdf = s
+		case 5:
+			twoThreeFive = append(twoThreeFive, s)
+		}
+	}
+	a := diffAdditions(cf, acf)
+	bd := diffAdditions(cf, bcdf)
+	abcdf := a + cf + bd
+
+	// abcdf -> "acdeg": 2, -> eg
+	// abcdf -> "acdfg": 3, -> g
+	// abcdf -> "abdfg": 5, -> g
+	var acdeg, eg, g string
+	for _, s := range twoThreeFive {
+		diff := diffAdditions(abcdf, s)
+		if len(diff) == 2 {
+			acdeg = s // 2
+			eg = diff
+		} else {
+			g = diff
+		}
 	}
 
-	sort.Slice(decodedRunes, func(i int, j int) bool { return decodedRunes[i] < decodedRunes[j] })
-	sortedDecoded := string(decodedRunes)
+	e := diffAdditions(g, eg)
+	aeg := a + e + g
+	cd := diffAdditions(aeg, acdeg)
+	b := diffAdditions(cd, bd)
+	d := diffAdditions(cf, cd)
+	f := diffAdditions(cd, cf)
+	c := diffAdditions(d, cd)
+
+	decodeTemplate = make(map[byte]byte, 0)
+	decodeTemplate[a[0]] = 'a'
+	decodeTemplate[b[0]] = 'b'
+	decodeTemplate[c[0]] = 'c'
+	decodeTemplate[d[0]] = 'd'
+	decodeTemplate[e[0]] = 'e'
+	decodeTemplate[f[0]] = 'f'
+	decodeTemplate[g[0]] = 'g'
+
+	return
+}
+
+func decodeDigit(code string, segmentsDecodeMap map[byte]byte) (digit int) {
+
+	decoded := make([]byte, 0)
+	for i := 0; i < len(code); i++ {
+		decoded = append(decoded, segmentsDecodeMap[code[i]])
+	}
+
+	sort.Slice(decoded, func(i int, j int) bool { return decoded[i] < decoded[j] })
+	sortedDecoded := string(decoded)
 
 	mapResults := map[string]int{
 		"abcefg":  0,
@@ -98,114 +150,6 @@ func decodeDigit(code string, segmentsDecodeMap map[rune]rune) (digit int) {
 		digit = val
 	} else {
 		// some error handling TODO
-	}
-
-	return
-}
-
-func createSignalWireToSegmentMap(digitToSignalPatternMap map[int]string) (segmentsDecodeMap map[rune]rune) {
-
-	segmentsDecodeMap = make(map[rune]rune, 7)
-	a := rune(diffAdditions(digitToSignalPatternMap[1], digitToSignalPatternMap[7])[0])
-	segmentsDecodeMap[a] = 'a'
-	b := rune(diffAdditions(digitToSignalPatternMap[3], digitToSignalPatternMap[5])[0])
-	segmentsDecodeMap[b] = 'b'
-	c := rune(diffAdditions(digitToSignalPatternMap[6], digitToSignalPatternMap[8])[0])
-	segmentsDecodeMap[c] = 'c'
-	d := rune(diffAdditions(digitToSignalPatternMap[0], digitToSignalPatternMap[8])[0])
-	segmentsDecodeMap[d] = 'd'
-	e := rune(diffAdditions(digitToSignalPatternMap[9], digitToSignalPatternMap[8])[0])
-	segmentsDecodeMap[e] = 'e'
-	f := rune(diffAdditions(digitToSignalPatternMap[2], digitToSignalPatternMap[3])[0])
-	segmentsDecodeMap[f] = 'f'
-
-	for _, r := range "abcdefg" {
-		if _, ok := segmentsDecodeMap[r]; !ok {
-			segmentsDecodeMap[r] = 'g'
-			break
-		}
-	}
-
-	return
-}
-
-func createDigitToSignalMap(tenPattern []string) (digitToSignalPatternMap map[int]string) {
-	digitToSignalPatternMap = make(map[int]string, 0)
-
-	twoThreeFive := make([]string, 0)
-	zeroSixNine := make([]string, 0)
-	for _, v := range tenPattern {
-		length := len(v)
-		switch length {
-		case 2:
-			digitToSignalPatternMap[1] = v
-		case 3:
-			digitToSignalPatternMap[7] = v
-		case 4:
-			digitToSignalPatternMap[4] = v
-		case 5:
-			twoThreeFive = append(twoThreeFive, v)
-		case 6:
-			zeroSixNine = append(zeroSixNine, v)
-		case 7:
-			digitToSignalPatternMap[8] = v
-		}
-	}
-	// 0 vs 8 = d
-	// 6 vs 8 = c
-	// 9 vs 8 = e
-	// -> Find Dif 8 vs 6-length ones gives us dce
-	dce := ""
-	for i := 0; i < len(zeroSixNine); i++ {
-		difference := diffAdditions(zeroSixNine[i], digitToSignalPatternMap[8])
-		dce += difference
-	}
-
-	// find 2, 3, 5
-	// 2 vs 5 -> 2 !!!
-	// 2 vs 3 -> 1
-	// 3 vs 5 -> 1
-	for i := 0; i < len(twoThreeFive); i++ {
-		for k := i + 1; k < len(twoThreeFive); k++ {
-			differences := len(diffAdditions(twoThreeFive[i], twoThreeFive[k]))
-			if differences == 2 {
-				// 2 contains all of dce
-				// other part of the pair is 5
-				if len(diffAdditions(twoThreeFive[i], dce)) == 0 {
-					digitToSignalPatternMap[2] = twoThreeFive[i]
-					digitToSignalPatternMap[5] = twoThreeFive[k]
-				} else {
-					digitToSignalPatternMap[2] = twoThreeFive[k]
-					digitToSignalPatternMap[5] = twoThreeFive[i]
-				}
-				// three is the remaining
-				for index := range twoThreeFive {
-					if index != i && index != k {
-						digitToSignalPatternMap[3] = twoThreeFive[index]
-					}
-				}
-			}
-		}
-	}
-
-	for _, v := range zeroSixNine {
-		// 5 vs 6 -> 1
-		// 5 vs 9 -> 1
-		// 5 vs 0 -> 2 !!!
-		difference := diffAdditions(digitToSignalPatternMap[5], v)
-		if len(difference) == 2 {
-			digitToSignalPatternMap[0] = v
-			continue
-		}
-
-		// 9 vs 7 -> 0 additions
-		// 6 vs 7 -> 1
-		difference = diffAdditions(v, digitToSignalPatternMap[7])
-		if len(difference) == 0 {
-			digitToSignalPatternMap[9] = v
-		} else {
-			digitToSignalPatternMap[6] = v
-		}
 	}
 
 	return
