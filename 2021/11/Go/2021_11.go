dd8bed9171d6eec04454da9e286661955c77440c
tinmanjk <tinmanjk@mail.com>
2021-12-11 09:50:57 +0200

    2021:11 Refactorings

--- 2021/11/Go/2021_11.go
+++ 2021/11/Go/2021_11.go
@@ -11,17 +11,14 @@ import (
 func main() {
 	lines := returnSliceOfLinesFromFile(inputPath)
 	var result int
+	parsedInput := parseInput(lines)
 
-	var parsedInput = parseInput(lines)
-	paddedInput := addPaddings(parsedInput, -1000)
 	// part 1
-	result = findResult(paddedInput, false)
+	result = findResult(parsedInput, 100, false)
 	fmt.Println(result)
 
-	// // part 2
-	parsedInput = parseInput(lines)
-	paddedInput = addPaddings(parsedInput, -1000)
-	result = findResult(paddedInput, true)
+	// part 2
+	result = findResult(parsedInput, 100, true)
 	fmt.Println(result)
 }
 
@@ -64,158 +61,146 @@ func addPaddings(solutionsData [][]int, paddedValue int) (paddedSolutionsData []
 	return
 }
 
-// energy levels of octopuses
-// flashes of light
-// single step
-// 1. ++ energy level
-// 2. if > 9 flashes
-// 3. all adjacent ++ -> diagonally adjacent
-// 3a -> back to 2 if adjacent rises to >9 - flashes - ad infinitum
-// flash -> set to 0...and remaining there I guess
+const inputPath = "../input.txt"
+const paddedValue = -1
 
-func produceFlashInitialMatrix(totalRows int, totalCols int) (flashMatrix [][]int) {
+const noFlashMarker = 0
+const toFlashMarker = 1
+const finishedFlashingMarker = 2
 
-	alreadyFlashedInitial := 1
+const flashThreshold = 9
+
+func findResult(inputData [][]int, numberSteps int, partTwo bool) (result int) {
+
+	paddedData := addPaddings(inputData, paddedValue)
+	inputDataRowCount := len(paddedData)
+	// assuming equal number of columns
+	inputDataColumnCount := len(paddedData[0])
+	flashState := makeFlashStateMatrix(inputDataRowCount, inputDataColumnCount)
+
+	waitingForSimultaneousFlash := partTwo
+	for step := 1; (step <= numberSteps) || waitingForSimultaneousFlash; step++ {
+		// 1. Step mark
+		energizeAndMark := func(i int, j int) (breaks bool, breakReturns bool, returnValue interface{}) {
+			energizeAndMark(paddedData, flashState, i, j)
+			return
+		}
+		iteratePaddedMatrix(paddedData, energizeAndMark)
+
+		// 2. NeighbourMarks iteratively
+		flashAndMarkNeighbors := func(i int, j int) (breaks bool, breakReturns bool, returnValue interface{}) {
+			if (flashState)[i][j] != toFlashMarker {
+				return // effectively continue
+			}
+			energizeAndMarkNeighbors(i, j, paddedData, flashState)
+			flashState[i][j] = finishedFlashingMarker
+			return
+		}
+
+		for !allFinishedFlashing(flashState) {
+			iteratePaddedMatrix(paddedData, flashAndMarkNeighbors)
+		}
+
+		// 3. Set Flashed Ones to 0
+		finalSweep := func(i int, j int) (breaks bool, breakReturns bool, returnVal interface{}) {
+			if flashState[i][j] == finishedFlashingMarker {
+				paddedData[i][j] = 0
+				flashState[i][j] = noFlashMarker
+				result++
+			}
+			return
+		}
+		iteratePaddedMatrix(paddedData, finalSweep)
+
+		if !partTwo {
+			continue
+		}
+
+		foundNonFlashIterator := func(i, j int) (breaks bool, breakReturns bool, returnValue interface{}) {
+			if paddedData[i][j] > 0 {
+				return true, true, true // breakreturn
+			}
+			return // continue
+		}
+		iterationBreakReturn, foundNonFlash := iteratePaddedMatrix(paddedData, foundNonFlashIterator)
+		if iterationBreakReturn && foundNonFlash.(bool) {
+			continue
+		}
+		return step
+	}
+	return
+}
+
+func makeFlashStateMatrix(totalRows int, totalCols int) (flashMatrix [][]int) {
+
 	flashMatrix = make([][]int, totalRows)
 	for i := 0; i < totalRows; i++ {
 		flashMatrix[i] = make([]int, totalCols)
 		for j := 0; j < totalCols; j++ {
-			flashMatrix[i][j] = alreadyFlashedInitial
+			flashMatrix[i][j] = noFlashMarker
 		}
 	}
 
 	return
 }
 
-func pulse(inputData [][]int, alreadyFlashed [][]int) (numbeFlashes int) {
+// return defaults = continue
+type iteratorSignature = func(i int, j int) (breaks bool, breakReturns bool, returnValue interface{})
 
-	for j := 1; j < len(alreadyFlashed)-1; j++ {
-		for k := 1; k < len(alreadyFlashed[j])-1; k++ {
-			if alreadyFlashed[j][k] == 0 {
-				// all positions
-				if inputData[j-1][k-1] != 10 {
-					inputData[j-1][k-1]++
-					if inputData[j-1][k-1] > 9 {
-						numbeFlashes++
-						alreadyFlashed[j-1][k-1] = 0
-					}
-				}
-				if inputData[j-1][k] != 10 {
-					inputData[j-1][k]++
-					if inputData[j-1][k] > 9 {
-						numbeFlashes++
-						alreadyFlashed[j-1][k] = 0
-					}
-				}
-				if inputData[j-1][k+1] != 10 {
-					inputData[j-1][k+1]++
-					if inputData[j-1][k+1] > 9 {
-						numbeFlashes++
-						alreadyFlashed[j-1][k+1] = 0
-					}
-				}
-				if inputData[j][k-1] != 10 {
-					inputData[j][k-1]++
-					if inputData[j][k-1] > 9 {
-						numbeFlashes++
-						alreadyFlashed[j][k-1] = 0
-					}
-				}
-				if inputData[j][k+1] != 10 {
-					inputData[j][k+1]++
-					if inputData[j][k+1] > 9 {
-						numbeFlashes++
-						alreadyFlashed[j][k+1] = 0
-					}
-				}
-				if inputData[j+1][k-1] != 10 {
-					inputData[j+1][k-1]++
-					if inputData[j+1][k-1] > 9 {
-						numbeFlashes++
-						alreadyFlashed[j+1][k-1] = 0
-					}
-				}
-				if inputData[j+1][k] != 10 {
-					inputData[j+1][k]++
-					if inputData[j+1][k] > 9 {
-						numbeFlashes++
-						alreadyFlashed[j+1][k] = 0
-					}
-				}
-				if inputData[j+1][k+1] != 10 {
-					inputData[j+1][k+1]++
-					if inputData[j+1][k+1] > 9 {
-						numbeFlashes++
-						alreadyFlashed[j+1][k+1] = 0
-					}
-				}
+func iteratePaddedMatrix(paddedMatrix [][]int, iterator iteratorSignature) (breakReturns bool, retValue interface{}) {
 
-				alreadyFlashed[j][k] = -1
+	rowNumber := len(paddedMatrix)
+	columnNumber := len(paddedMatrix[0])
+
+outerLoop:
+	for j := 1; j < rowNumber-1; j++ {
+		for k := 1; k < columnNumber-1; k++ {
+			breaks, breakReturns, retVal := iterator(j, k)
+			// if blank return continue
+			if breakReturns {
+				return breakReturns, retVal
+			}
+			if breaks {
+				break outerLoop
 			}
 		}
 	}
-	return
+	return // false, nil
 }
 
-func IsFinished(alreadyFlashed [][]int) bool {
-	for j := 1; j < len(alreadyFlashed)-1; j++ {
-		for k := 1; k < len(alreadyFlashed[j])-1; k++ {
-			if alreadyFlashed[j][k] == 0 {
-				return false
-			}
+func energizeAndMark(inputData [][]int, flashMatrix [][]int, i int, j int) {
+	if flashMatrix[i][j] == noFlashMarker {
+		inputData[i][j]++
+		if inputData[i][j] > flashThreshold {
+			flashMatrix[i][j] = toFlashMarker
 		}
 	}
-	return true
 }
 
-const inputPath = "../input.txt"
-
-func findResult(inputData [][]int, partTwo bool) (result int) {
-
-	// alreadyFlashed
-	// full of 1s
-	alreadyFlashed := produceFlashInitialMatrix(len(inputData), len(inputData))
-	waitingForAllFlash := partTwo
-	// total flashes after 100 steps
-	for i := 0; (i < 100) || waitingForAllFlash; i++ {
-		if partTwo {
-			sim := 0
-			for j := 1; j < len(inputData)-1; j++ {
-				for k := 1; k < len(inputData[j])-1; k++ {
-					sim += inputData[j][k]
-				}
+func energizeAndMarkNeighbors(i int, j int, inputData [][]int, flashMatrix [][]int) {
+	// neighbor offsets
+	for k := -1; k <= 1; k++ {
+		for m := -1; m <= 1; m++ {
+			if k == 0 && m == 0 {
+				continue
 			}
-			if sim == 0 {
-				return i
-			}
+			energizeAndMark(inputData, flashMatrix, i+k, j+m)
 		}
+	}
+}
 
-		// 1. increase everybody and first Flash 2.
-		for j := 1; j < len(inputData)-1; j++ {
-			for k := 1; k < len(inputData[j])-1; k++ {
-				inputData[j][k]++
-				if inputData[j][k] > 9 {
-					result++
-					// FLASH record
-					// assume that it stays there!!!!!
-					alreadyFlashed[j][k] = 0 // good becauase that's
-					// inputData[j][k] = 0
-				}
-			}
-		}
+func allFinishedFlashing(flashMatrix [][]int) (result bool) {
 
-		for !IsFinished(alreadyFlashed) {
-			result += pulse(inputData, alreadyFlashed)
-		}
-		// 3. all adjacent ++
-		for j := 1; j < len(inputData)-1; j++ {
-			for k := 1; k < len(inputData[j])-1; k++ {
-				if alreadyFlashed[j][k] == -1 {
-					inputData[j][k] = 0
-				}
+	result = true
+	breakReturns, anyToFlash := iteratePaddedMatrix(flashMatrix,
+		func(i, j int) (breaks bool, breakReturns bool, atLeastOneToFlash interface{}) {
+			if flashMatrix[i][j] == toFlashMarker {
+				return true, true, true
 			}
-		}
-		alreadyFlashed = produceFlashInitialMatrix(len(inputData), len(inputData))
+			return //false, false, nil
+		})
+	if breakReturns {
+		result = !anyToFlash.(bool)
 	}
 	return
 }
