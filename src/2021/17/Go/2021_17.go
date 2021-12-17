e12265c42a84c3bdddd24c28f0932950c14ee432
tinmanjk <tinmanjk@mail.com>
2021-12-18 00:06:55 +0200

    2021:17 Removed debugging helpers

--- src/2021/17/Go/2021_17.go
+++ src/2021/17/Go/2021_17.go
@@ -4,7 +4,6 @@ import (
 	"aoc/libs/go/inputParse"
 	"fmt"
 	"math"
-	"sort"
 	"strconv"
 	"strings"
 )
@@ -14,10 +13,10 @@ func main() {
 	var result int
 	targetArea := parseInput(lines)
 	// part 1
-	result = findResult(targetArea, false, true)
+	result = findResult(targetArea, false)
 	fmt.Println(result)
 	// part 2
-	result = findResult(targetArea, true, true)
+	result = findResult(targetArea, true)
 	fmt.Println(result)
 }
 
@@ -117,15 +116,9 @@ func calcTravelX(initialVelocity int, stepsToMake int) (coorX int, finalVelocity
 	return
 }
 
-func findResult(targetArea TargetArea, partTwo bool, debug bool) (result int) {
+func findResult(targetArea TargetArea, partTwo bool) (result int) {
 
 	validXVelocitiesSteps := findValidXVelocities(targetArea)
-
-	if debug {
-		validxOnlySorted := debugGetSortedValidXOnly(validXVelocitiesSteps)
-		fmt.Println(validxOnlySorted)
-		// 6-15 vkluchitelno 20-30 vkluchitelno -> MATCH the ShouldVelocities
-	}
 
 	velocitiesMap := map[Velocity]Velocity{}
 	highestY := math.MinInt32
@@ -168,59 +161,10 @@ func findResult(targetArea TargetArea, partTwo bool, debug bool) (result int) {
 
 	}
 
-	if debug {
-		fmt.Println(" ")
-		shouldVelocities := []Velocity{
-			{23, -10}, {25, -9}, {27, -5}, {29, -6}, {22, -6}, {21, -7}, {9, 0}, {27, -7}, {24, -5},
-			{25, -7}, {26, -6}, {25, -5}, {6, 8}, {11, -2}, {20, -5}, {29, -10}, {6, 3}, {28, -7},
-			{8, 0}, {30, -6}, {29, -8}, {20, -10}, {6, 7}, {6, 4}, {6, 1}, {14, -4}, {21, -6},
-			{26, -10}, {7, -1}, {7, 7}, {8, -1}, {21, -9}, {6, 2}, {20, -7}, {30, -10}, {14, -3},
-			{20, -8}, {13, -2}, {7, 3}, {28, -8}, {29, -9}, {15, -3}, {22, -5}, {26, -8}, {25, -8},
-			{25, -6}, {15, -4}, {9, -2}, {15, -2}, {12, -2}, {28, -9}, {12, -3}, {24, -6}, {23, -7},
-			{25, -10}, {7, 8}, {11, -3}, {26, -7}, {7, 1}, {23, -9}, {6, 0}, {22, -10}, {27, -6},
-			{8, 1}, {22, -8}, {13, -4}, {7, 6}, {28, -6}, {11, -4}, {12, -4}, {26, -9}, {7, 4},
-			{24, -10}, {23, -8}, {30, -8}, {7, 0}, {9, -1}, {10, -1}, {26, -5}, {22, -9}, {6, 5},
-			{7, 5}, {23, -6}, {28, -10}, {10, -2}, {11, -1}, {20, -9}, {14, -2}, {29, -7}, {13, -3},
-			{23, -5}, {24, -8}, {27, -9}, {30, -7}, {28, -5}, {21, -10}, {7, 9}, {6, 6}, {21, -5},
-			{27, -10}, {7, 2}, {30, -9}, {21, -8}, {22, -7}, {24, -9}, {20, -6}, {6, 9}, {29, -5},
-			{8, -2}, {27, -8}, {30, -5}, {24, -7},
-		}
-		fmt.Printf("Should velocities count %v\n", len(shouldVelocities))
-
-		myWrong := []Velocity{}
-		for _, mv := range velocitiesMap {
-			found := false
-			for _, sv := range shouldVelocities {
-				if sv == mv {
-					found = true
-					break
-				}
-			}
-			if !found {
-				myWrong = append(myWrong, mv)
-			}
-		}
-
-		fmt.Printf("My velocities wrong %v\n", len(myWrong))
-		fmt.Printf("My velocities count %v\n", len(velocitiesMap))
-
-		fmt.Println(" ")
-	}
-
 	if partTwo {
 		return len(velocitiesMap)
 	}
 
 	result = highestY * (highestY + 1) / 2
-	return
-}
-
-func debugGetSortedValidXOnly(validXVelocityStep map[int][]int) (validXOnlySorted []int) {
-
-	for k := range validXVelocityStep {
-		validXOnlySorted = append(validXOnlySorted, k)
-	}
-
-	sort.Ints(validXOnlySorted)
 	return
 }
