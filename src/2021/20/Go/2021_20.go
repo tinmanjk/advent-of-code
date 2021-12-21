d379dbb8476fccf06143898d9ddb5f9e98daf481
tinmanjk <tinmanjk@mail.com>
2021-12-21 22:26:46 +0200

    2021:20 input parsing and determination of restofInfinite pixel after each time

--- src/2021/20/Go/2021_20.go
+++ src/2021/20/Go/2021_20.go
@@ -21,16 +21,10 @@ func main() {
 
 func parseInput(lines []string) (imageEnhancement []rune, inputImage [][]rune) {
 
-	// 512 chars long
-	// line 1 = image inhancement algorithm
-	// can match each 9bit binary number
-	// 2 to the 9
-
-	// image
-	// two-dimensional array
-	// light pixels - #
-	// dark pixels - .
+	imageEnhancement = []rune(lines[0])
 	for i := 2; i < len(lines); i++ {
+		lineToRune := []rune(lines[i])
+		inputImage = append(inputImage, lineToRune)
 	}
 	return
 }
@@ -40,18 +34,49 @@ const inputPath = "../input0.txt"
 func findResult(imageEnhancement []rune, inputImage [][]rune) (result int) {
 
 	outputImage := [][]rune{}
-	for i := 0; i < 2; i++ {
-		outputImage = enhanceImage(imageEnhancement, inputImage)
+	for i := 0; i < 10; i++ {
+		outputImage = enhanceImage(imageEnhancement, inputImage, i)
 	}
-	// pad TWICE...i taka
-	// padding sas sigurnost
-	// za da stane INFINITE IMAGE..bla
 
 	return len(outputImage)
 }
 
-func enhanceImage(imageEnhancement []rune, inputImage [][]rune) (outputImage [][]rune) {
-	wipImage := matrixHelpers.AddPaddingsWithRunes(inputImage, '.')
+func determineRestOfInfinite(time int, imageEnhancement []rune) (restOfInfinite rune) {
+	if time == 0 {
+		restOfInfinite = '.'
+	}
 
-	return wipImage
+	if time == 1 {
+		restOfInfinite = imageEnhancement[0] // sum of ... ... .. = 0
+	}
+
+	if time >= 2 {
+		if imageEnhancement[0] == '.' {
+			restOfInfinite = '.' // stays there forever
+		}
+
+		if imageEnhancement[0] == '#' && imageEnhancement[len(imageEnhancement)-1] == '#' {
+			restOfInfinite = '#' // stays there forever
+		}
+
+		// alternating
+		if imageEnhancement[0] == '#' && imageEnhancement[len(imageEnhancement)-1] == '.' {
+			if time%2 == 0 {
+				restOfInfinite = imageEnhancement[len(imageEnhancement)-1] // '.'
+			} else {
+				restOfInfinite = imageEnhancement[0] // '#'
+			}
+		}
+	}
+
+	return
+}
+
+func enhanceImage(imageEnhancement []rune, inputImage [][]rune, time int) (outputImage [][]rune) {
+
+	restOfInfinite := determineRestOfInfinite(time, imageEnhancement)
+
+	adjacentInputImage := matrixHelpers.AddPaddingsWithRunes(inputImage, restOfInfinite)
+
+	return adjacentInputImage
 }
