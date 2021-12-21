d2ad490a12aa888b77e80e85ea86e1f7f7dc7877
tinmanjk <tinmanjk@mail.com>
2021-12-21 21:32:07 +0200

    2021:20 Initial func structure

--- src/2021/20/Go/2021_20.go
+++ src/2021/20/Go/2021_20.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"aoc/libs/go/inputParse"
+	"aoc/libs/go/matrixHelpers"
 	"fmt"
 )
 
 func main() {
 	lines := inputParse.ReturnSliceOfLinesFromFile(inputPath)
+	imageEnhancement, inputImage := parseInput(lines)
 	var result int
 	// part 1
-	result = findResult(lines)
+	result = findResult(imageEnhancement, inputImage)
 	fmt.Println(result)
 
 	// // part 2
@@ -17,16 +19,39 @@ func main() {
 	// fmt.Println(result)
 }
 
-func parseInput(lines []string) {
+func parseInput(lines []string) (imageEnhancement []rune, inputImage [][]rune) {
 
-	for i := 0; i < len(lines); i++ {
+	// 512 chars long
+	// line 1 = image inhancement algorithm
+	// can match each 9bit binary number
+	// 2 to the 9
+
+	// image
+	// two-dimensional array
+	// light pixels - #
+	// dark pixels - .
+	for i := 2; i < len(lines); i++ {
 	}
 	return
 }
 
 const inputPath = "../input0.txt"
 
-func findResult(lines []string) (result int) {
+func findResult(imageEnhancement []rune, inputImage [][]rune) (result int) {
 
-	return
+	outputImage := [][]rune{}
+	for i := 0; i < 2; i++ {
+		outputImage = enhanceImage(imageEnhancement, inputImage)
+	}
+	// pad TWICE...i taka
+	// padding sas sigurnost
+	// za da stane INFINITE IMAGE..bla
+
+	return len(outputImage)
+}
+
+func enhanceImage(imageEnhancement []rune, inputImage [][]rune) (outputImage [][]rune) {
+	wipImage := matrixHelpers.AddPaddingsWithRunes(inputImage, '.')
+
+	return wipImage
 }
