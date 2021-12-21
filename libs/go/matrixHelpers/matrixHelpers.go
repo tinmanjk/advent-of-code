190d1f0f924de5ff8a2005d7512127a9c00c976d
tinmanjk <tinmanjk@mail.com>
2021-12-21 22:59:27 +0200

    2021: added paddings helpers for 20

---
+++ libs/go/matrixHelpers/matrixHelpers.go
@@ -0,0 +1,51 @@
+package matrixHelpers
+
+func AddPaddings(solutionsData [][]int, paddedValue int) (paddedSolutionsData [][]int) {
+
+	paddedSolutionsData = make([][]int, len(solutionsData))
+	for i := 0; i < len(solutionsData); i++ {
+		paddedSolutionsData[i] = append([]int{paddedValue}, solutionsData[i]...)
+		paddedSolutionsData[i] = append(paddedSolutionsData[i], paddedValue)
+	}
+
+	lenghtPaddedSingleLine := len(solutionsData[0]) + 2 // should be the same for all
+	paddedBeginRow := make([]int, lenghtPaddedSingleLine)
+	for i := 0; i < len(paddedBeginRow); i++ {
+		paddedBeginRow[i] = paddedValue
+	}
+	paddedEndRow := make([]int, lenghtPaddedSingleLine)
+	copy(paddedEndRow, paddedBeginRow)
+
+	paddedMatrix := make([][]int, 0)
+	paddedMatrix = append(paddedMatrix, paddedBeginRow)
+
+	paddedSolutionsData = append(paddedMatrix, paddedSolutionsData...)
+	paddedSolutionsData = append(paddedSolutionsData, paddedEndRow)
+
+	return
+}
+
+func AddPaddingsWithRunes(solutionsData [][]rune, paddedValue rune) (paddedSolutionsData [][]rune) {
+
+	paddedSolutionsData = make([][]rune, len(solutionsData))
+	for i := 0; i < len(solutionsData); i++ {
+		paddedSolutionsData[i] = append([]rune{paddedValue}, solutionsData[i]...)
+		paddedSolutionsData[i] = append(paddedSolutionsData[i], paddedValue)
+	}
+
+	lenghtPaddedSingleLine := len(solutionsData[0]) + 2 // should be the same for all
+	paddedBeginRow := make([]rune, lenghtPaddedSingleLine)
+	for i := 0; i < len(paddedBeginRow); i++ {
+		paddedBeginRow[i] = paddedValue
+	}
+	paddedEndRow := make([]rune, lenghtPaddedSingleLine)
+	copy(paddedEndRow, paddedBeginRow)
+
+	paddedMatrix := make([][]rune, 0)
+	paddedMatrix = append(paddedMatrix, paddedBeginRow)
+
+	paddedSolutionsData = append(paddedMatrix, paddedSolutionsData...)
+	paddedSolutionsData = append(paddedSolutionsData, paddedEndRow)
+
+	return
+}
